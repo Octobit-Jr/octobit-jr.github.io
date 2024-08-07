e6f6716d429a642f79bbefa5f6c5e1e707e5934b
Erick Oda Coulter <erickodac@hotmail.com>
2024-08-07 00:56:48 -0300

    feat: added forms

---
+++ src/scripts/utils/masks/phoneMask.js
@@ -0,0 +1,9 @@
+const phoneMask = (value) => {
+  if (!value) return "";
+  value = value.replace(/\D/g, "");
+  value = value.replace(/(\d{2})(\d)/, "($1) $2");
+  value = value.replace(/(\d)(\d{4})$/, "$1-$2");
+  return value;
+};
+
+export default phoneMask;
